8813d7bf6c582ad3666bdb2e54389b2cff949405
wf975799 <2478729154@qq.com>
2017-11-28 17:19:34 +0800

    王飞飞提交作业

---
+++ Struts2/T2/王飞飞/struts2T2/src/com/qhit/action/action/ssAction.java
@@ -0,0 +1,30 @@
+package com.lmy.action.action;
+
+import com.opensymphony.xwork2.Action;
+
+
+public class ssAction implements Action {
+			private String Content;
+			private String uname;
+			@Override
+			public String execute() throws Exception {
+				// TODO Auto-generated method stub
+				Content = "hello"+uname;
+				return Action.SUCCESS;
+			}
+			public String getContent() {
+				return Content;
+			}
+			public void setContent(String content) {
+				Content = content;
+			}
+			public String getUname() {
+				return uname;
+			}
+			public void setUname(String uname) {
+				this.uname = uname;
+			}
+
+
+			
+}
---
+++ Struts2/T2/王飞飞/struts2T2/src/com/qhit/action/interceptor/print.java
@@ -0,0 +1,33 @@
+package com.lmy.action.interceptor;
+
+import javax.xml.ws.spi.Invoker;
+
+import org.eclipse.jdt.internal.compiler.ast.Invocation;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class print implements Interceptor {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("自定义销毁");
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		System.out.println("自定义初始化");
+	}
+
+	@Override
+	public String intercept(ActionInvocation arg0) throws Exception {
+		// TODO Auto-generated method stub
+		String result = arg0.invoke();
+		System.out.println("返回代码"+result);
+		System.out.println("自定义执行");
+		return result;
+	}
+
+}
